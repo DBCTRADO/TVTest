38fc2fc02e18db708b20832457559661a63329d3
DBCTRADO <hdustest@gmail.com>
2015-02-14 20:13:59 +0900

    行末禁則文字に▽と▼を追加

--- src/TextDraw.cpp
+++ src/TextDraw.cpp
@@ -19,7 +19,7 @@ const LPCWSTR CTextDraw::m_pszStartProhibitChars=
 	L")）]］｣」』】〉》”、。,，.．!！?？ー～…・ァィゥェォッャュョヮヵヶぁぃぅぇぉっゃゅょゎゝゞ々";
 // 行末禁則文字
 const LPCWSTR CTextDraw::m_pszEndProhibitChars=
-	L"(（[［｢「『【〈《“#＃";
+	L"(（[［｢「『【〈《“#＃▽▼";
 
 
 CTextDraw::CTextDraw()
