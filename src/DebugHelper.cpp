b0655743780037c0bdb7d40d71496a43e1a0153e
DBCTRADO <hdustest@gmail.com>
2017-10-06 21:04:43 +0900

    Release ビルドで変数未定義のエラーになるのを修正 (since b7e76720d3403c84fddd3a20ea98bf0fc55f2e73)

--- src/DebugHelper.cpp
+++ src/DebugHelper.cpp
@@ -352,7 +352,8 @@ int CDebugHelper::FormatSymbolFromAddress(
 		}
 	}
 #else
-	for (int i = 0; i < NumModuleEntries; i++) {
+	int i;
+	for (i = 0; i < NumModuleEntries; i++) {
 		if (Address >= (DWORD64)pModuleEntries[i].modBaseAddr
 				&& Address < (DWORD64)pModuleEntries[i].modBaseAddr + pModuleEntries[i].modBaseSize) {
 			pszModule = pModuleEntries[i].szModule;
