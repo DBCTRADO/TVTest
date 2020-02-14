ab5d0f0825ec87b2fafdd076d96f76ebef954bc2
DBCTRADO <hdustest@gmail.com>
2020-02-14 21:27:51 +0900

    Copyright 表記の年を2020に更新

--- src/UICore.cpp
+++ src/UICore.cpp
@@ -1,6 +1,6 @@
 /*
   TVTest
-  Copyright(c) 2008-2019 DBCTRADO
+  Copyright(c) 2008-2020 DBCTRADO
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
