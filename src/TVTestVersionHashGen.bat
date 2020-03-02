9b0e213cc4766f57216012f20c716cbe54088f10
DBCTRADO <hdustest@gmail.com>
2020-03-02 23:36:45 +0900

    TVTestVersionHash.h の更新を必要な時のみ行う

--- src/TVTestVersionHashGen.bat
+++ src/TVTestVersionHashGen.bat
@@ -2,9 +2,12 @@
 cd /d %~dp0
 set headerfile=TVTestVersionHash.h
 for /f "usebackq tokens=*" %%i in (`git rev-parse --short HEAD`) do set hash=%%i
-if not "%hash%"=="" (
+if "%hash%"=="" (
+	if exist %headerfile% del %headerfile%
+	exit /b 0
+)
+find "%hash%" %headerfile% >nul
+if %errorlevel% neq 0 (
 	echo #define VERSION_HASH_A "%hash%">%headerfile%
-) else if exist %headerfile% (
-	del %headerfile%
 )
 exit /b 0
