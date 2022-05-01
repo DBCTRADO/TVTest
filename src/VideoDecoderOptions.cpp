0ff4e3cfeb3101f30225c2a7d41152f7d9bbfd58
DBCTRADO <hdustest@gmail.com>
2022-05-01 21:59:25 +0900

    TVTest DTV Video Decoder の設定の保存と読み込みにプロパティを使用

--- src/VideoDecoderOptions.cpp
+++ src/VideoDecoderOptions.cpp
@@ -1,6 +1,6 @@
 /*
   TVTest
-  Copyright(c) 2008-2020 DBCTRADO
+  Copyright(c) 2008-2022 DBCTRADO
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -54,27 +54,66 @@ bool CVideoDecoderOptions::ReadSettings(CSettings &Settings)
 	Settings.Read(TEXT("EnableD3D11"), &m_VideoDecoderSettings.bEnableD3D11);
 	Settings.Read(TEXT("NumQueueFrames"), &m_VideoDecoderSettings.NumQueueFrames);
 
+	CSettings::EntryList EntryList;
+	if (Settings.GetEntries(&EntryList)) {
+		m_VideoDecoderSettings.Properties.clear();
+		m_VideoDecoderSettings.Properties.reserve(EntryList.size());
+
+		for (const auto &Entry : EntryList) {
+			if (Entry.Value.empty())
+				continue;
+
+			auto &Property = m_VideoDecoderSettings.Properties.emplace_back();
+
+			Property.Name = Entry.Name;
+
+			if (::lstrcmpi(Entry.Value.c_str(), TEXT("yes")) == 0
+					|| ::lstrcmpi(Entry.Value.c_str(), TEXT("true")) == 0) {
+				Property.Value.vt = VT_BOOL;
+				Property.Value.boolVal = VARIANT_TRUE;
+			} else if (::lstrcmpi(Entry.Value.c_str(), TEXT("no")) == 0
+					|| ::lstrcmpi(Entry.Value.c_str(), TEXT("false")) == 0) {
+				Property.Value.vt = VT_BOOL;
+				Property.Value.boolVal = VARIANT_FALSE;
+			} else {
+				Property.Value.vt = VT_INT;
+				Property.Value.intVal = ::StrToInt(Entry.Value.c_str());
+			}
+		}
+	}
+
 	return true;
 }
 
 
 bool CVideoDecoderOptions::WriteSettings(CSettings &Settings)
 {
-	Settings.Write(TEXT("EnableDeinterlace"), m_VideoDecoderSettings.bEnableDeinterlace);
-	Settings.Write(TEXT("DeinterlaceMethod"), static_cast<int>(m_VideoDecoderSettings.DeinterlaceMethod));
-	Settings.Write(TEXT("AdaptProgressive"), m_VideoDecoderSettings.bAdaptProgressive);
-	Settings.Write(TEXT("AdaptTelecine"), m_VideoDecoderSettings.bAdaptTelecine);
-	Settings.Write(TEXT("SetInterlacedFlag"), m_VideoDecoderSettings.bSetInterlacedFlag);
-	Settings.Write(TEXT("Brightness"), m_VideoDecoderSettings.Brightness);
-	Settings.Write(TEXT("Contrast"), m_VideoDecoderSettings.Contrast);
-	Settings.Write(TEXT("Hue"), m_VideoDecoderSettings.Hue);
-	Settings.Write(TEXT("Saturation"), m_VideoDecoderSettings.Saturation);
-	Settings.Write(TEXT("NumThreads"), m_VideoDecoderSettings.NumThreads);
-	Settings.Write(TEXT("EnableDXVA2"), m_VideoDecoderSettings.bEnableDXVA2);
-	Settings.Write(TEXT("EnableD3D11"), m_VideoDecoderSettings.bEnableD3D11);
-#if 0 // 設定UI未実装
-	Settings.Write(TEXT("NumQueueFrames"), m_VideoDecoderSettings.NumQueueFrames);
-#endif
+	if (!m_VideoDecoderSettings.Properties.empty()) {
+		for (const auto &Property : m_VideoDecoderSettings.Properties) {
+			switch (Property.Value.vt) {
+			case VT_INT:
+				Settings.Write(Property.Name.c_str(), Property.Value.intVal);
+				break;
+			case VT_BOOL:
+				Settings.Write(Property.Name.c_str(), Property.Value.boolVal != VARIANT_FALSE);
+				break;
+			}
+		}
+	} else {
+		Settings.Write(TEXT("EnableDeinterlace"), m_VideoDecoderSettings.bEnableDeinterlace);
+		Settings.Write(TEXT("DeinterlaceMethod"), static_cast<int>(m_VideoDecoderSettings.DeinterlaceMethod));
+		Settings.Write(TEXT("AdaptProgressive"), m_VideoDecoderSettings.bAdaptProgressive);
+		Settings.Write(TEXT("AdaptTelecine"), m_VideoDecoderSettings.bAdaptTelecine);
+		Settings.Write(TEXT("SetInterlacedFlag"), m_VideoDecoderSettings.bSetInterlacedFlag);
+		Settings.Write(TEXT("Brightness"), m_VideoDecoderSettings.Brightness);
+		Settings.Write(TEXT("Contrast"), m_VideoDecoderSettings.Contrast);
+		Settings.Write(TEXT("Hue"), m_VideoDecoderSettings.Hue);
+		Settings.Write(TEXT("Saturation"), m_VideoDecoderSettings.Saturation);
+		Settings.Write(TEXT("NumThreads"), m_VideoDecoderSettings.NumThreads);
+		Settings.Write(TEXT("EnableDXVA2"), m_VideoDecoderSettings.bEnableDXVA2);
+		Settings.Write(TEXT("EnableD3D11"), m_VideoDecoderSettings.bEnableD3D11);
+		Settings.Write(TEXT("NumQueueFrames"), m_VideoDecoderSettings.NumQueueFrames);
+	}
 
 	return true;
 }
